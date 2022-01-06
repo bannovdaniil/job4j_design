fb63e221f4ec1bc7a7de5ee908e9aceeee3be3fb
bannovdaniil <baddan@mail.ru>
2022-01-06 20:24:07 +0300

    2. Без переопределения equals и hashCode [#1005]

---
+++ src/main/java/ru/job4j/map/CollisionShow.java
@@ -0,0 +1,21 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollisionShow {
+    public static void main(String[] args) {
+        User user1 = new User("Семён", 3,
+                new GregorianCalendar(2022, 0, 6));
+        User user2 = new User("Семён", 3,
+                new GregorianCalendar(2022, 0, 6));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        for (var u : map.keySet()) {
+            System.out.println(u.hashCode() + ":" + u + ": " + map.get(u));
+        }
+    }
+}
