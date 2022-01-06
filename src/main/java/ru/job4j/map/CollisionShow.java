7596aa8ef86c5089f1cb7cab1c086ef299304a73
bannovdaniil <baddan@mail.ru>
2022-01-06 20:41:42 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/CollisionShow.java
+++ src/main/java/ru/job4j/map/CollisionShow.java
@@ -8,12 +8,13 @@ import java.util.Map;
 public class CollisionShow {
     public static void main(String[] args) {
         User user1 = new User("Семён", 3,
-                new GregorianCalendar(2022, 0, 6));
+                new GregorianCalendar(2022, Calendar.JANUARY, 6));
         User user2 = new User("Семён", 3,
-                new GregorianCalendar(2022, 0, 6));
+                new GregorianCalendar(2022, Calendar.JANUARY, 6));
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
+        System.out.println(map);
         for (var u : map.keySet()) {
             System.out.println(u.hashCode() + ":" + u + ": " + map.get(u));
         }
