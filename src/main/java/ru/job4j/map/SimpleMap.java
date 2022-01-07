1dd7ec2c0cf79c54f83c5be045410478cdb31f81
bannovdaniil <baddan@mail.ru>
2022-01-07 19:16:11 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008 #247940]

--- src/main/java/ru/job4j/map/SimpleMap.java
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -15,7 +15,7 @@ public class SimpleMap<K, V> implements Map<K, V> {
     @Override
     public boolean put(K key, V value) {
         boolean res = false;
-        if (count == capacity * LOAD_FACTOR) {
+        if (count >= capacity * LOAD_FACTOR) {
             expand();
         }
         int index = indexFor(key.hashCode());
