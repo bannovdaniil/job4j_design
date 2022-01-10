03aac587a57691521abb555234e671886a7d9e87
bannovdaniil <baddan@mail.ru>
2022-01-10 23:46:57 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/searchvisitor/Search.java
+++ src/main/java/ru/job4j/io/searchvisitor/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io.searchvisitor;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +10,30 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException(System.lineSeparator()
+                    + "Usage: java -jar Search.jar <FOLDER> <EXT>"
+                    + System.lineSeparator()
+                    + "Example: java -jar Search.jar c:\\ java");
+        }
+        File pathDir = new File(args[0]);
+        if (!pathDir.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s",
+                    pathDir.getAbsoluteFile()));
+        }
+        if (!pathDir.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s",
+                    pathDir.getAbsoluteFile()));
+        }
+        String extFile = args[1];
+        if (extFile.matches(".*[<>:\"\\|\\?\\*\\.].*")) {
+            throw new IllegalArgumentException(
+                    String.format("EXT [%s] contains one of the chars: .<>:\"|?*", extFile));
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName()
+                .endsWith("." + extFile))
+                .forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
