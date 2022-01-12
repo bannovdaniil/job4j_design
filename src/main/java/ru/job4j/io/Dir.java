1a4e81ddfbd7590c27fa4357e8aaaa1fe005d370
Daniil Bannov#14 <baddan@mail.ru>
2022-01-12 08:59:16 +0300

    5. Валидация параметров запуска. [#246865 #249223]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -7,7 +7,27 @@ public class Dir {
         if (args.length == 0) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
-        File file = new File(args[0]);
+        File file = checkFile(args[0]);
+        for (File subfile : file.listFiles()) {
+            if (subfile.isFile()) {
+                System.out.printf("Name: %s Size : %s%n",
+                        subfile.getName(),
+                        subfile.length());
+            }
+        }
+    }
+
+    /**
+     * Проводит проверку директории, если ошибки тогда бросаем исключение.
+     * 1. такая область на диске есть
+     * 2. эта область папка.
+     * Если все хорошо возвращаем дескриптор.
+     *
+     * @param folderName - аргументы командной строки.
+     * @return - File
+     */
+    private static File checkFile(String folderName) {
+        File file = new File(folderName);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s",
                     file.getAbsoluteFile()));
@@ -17,12 +37,6 @@ public class Dir {
                     file.getAbsoluteFile()));
         }
         System.out.printf("size : %s%n", file.getTotalSpace());
-        for (File subfile : file.listFiles()) {
-            if (subfile.isFile()) {
-                System.out.printf("Name: %s Size : %s%n",
-                        subfile.getName(),
-                        subfile.length());
-            }
-        }
+        return file;
     }
 }
