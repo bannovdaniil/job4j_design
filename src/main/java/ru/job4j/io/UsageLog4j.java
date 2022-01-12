d584070b81f04848e9e8208198f9157f3faca7e5
bannovdaniil <baddan@mail.ru>
2022-01-12 21:43:15 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -2,6 +2,10 @@ package ru.job4j.io;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.job4j.list.linkedlist.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * log4j:
@@ -13,10 +17,35 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte b = 1;
+        char c = 'a';
+        short s = 1;
+        int i = 1;
+        long l = 1;
+        float f = 0.1f;
+        double d = 0.1d;
+        LOG.debug("byte: {}, char: {}, short: {}, int: {}, long: {}, float: {}, double: {}",
+                b, c, s, i, l, f, d);
+        var user = new Object() {
+            private final String name = "Inline object name.";
+        };
+        LOG.debug("Object : {}, user.name : {}", user, user.name);
+        String name = "Petr Arsentev";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        double pi = 3.14159;
+        LOG.debug("Pi constant {}", pi);
+        int square = 20;
+        LOG.debug("Square {}", square);
+        int count = 5;
+        String fruit = "mango";
+        StringBuilder animal = new StringBuilder("Monkey");
+        LOG.debug("animal: {}, eat: {}, cont: {}", animal, fruit, count);
+        boolean result = false;
+        LOG.debug("Result of operation: {}", result);
+        ArrayList<String> list = new ArrayList<>();
+        LOG.debug("List: {}", list);
+        list.add("new string");
+        LOG.debug("List: {} size: {}", list, list.size());
     }
 }
