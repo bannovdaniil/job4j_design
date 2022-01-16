2348212578b928dc5f2d39b653c889d4fbc0c582
bannovdaniil <baddan@mail.ru>
2022-01-16 15:53:44 +0300

    0. Select [#6861 #251434]

--- sql/fauna/fauna_create.sql
+++ sql/fauna/fauna_create.sql
@@ -5,9 +5,9 @@ create table fauna (
     discovery_date date
 );
 
-insert into fauna(name, avg_age, discovery_date) values("Æèğàô", 15,  date '2014-09-04');
-insert into fauna(name, avg_age, discovery_date) values("Ñëîí", 20,  date '2017-10-01');
-insert into fauna(name, avg_age, discovery_date) values("Êîçà", 12,  date '2012-12-02');
-insert into fauna(name, avg_age, discovery_date) values("Êóğèöà", 5,  date '2018-05-11');
-insert into fauna(name, avg_age, discovery_date) values("golden fish chine", 10,  date '2019-04-21');
-insert into fauna(name, avg_age, discovery_date) values("Òèãğ", 9,  date '2022-01-10');
+insert into fauna(name, avg_age, discovery_date) values('Ğ–Ğ¸Ñ€Ğ°Ñ„', 15000,  date '1014-09-04');
+insert into fauna(name, avg_age, discovery_date) values('Ğ¡Ğ»Ğ¾Ğ½', 21000,  date '817-10-01');
+insert into fauna(name, avg_age, discovery_date) values('ĞšĞ¾Ğ·Ğ°', 12000,  date '1912-12-02');
+insert into fauna(name, avg_age, discovery_date) values('ĞšÑƒÑ€Ğ¸Ñ†Ğ°', 5000,  date '2018-05-11');
+insert into fauna(name, avg_age, discovery_date) values('golden fish chine', 10,  date '2019-04-21');
+insert into fauna(name, avg_age, discovery_date) values('Ğ¢Ğ¸Ğ³Ñ€', 9,  date '2022-01-10');
