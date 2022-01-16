a4e265292b78647037455a68c9b7503a7835613b
bannovdaniil <baddan@mail.ru>
2022-01-16 13:58:42 +0300

    0. Select [#6861 #251434]

--- sql/fauna/fauna.sql
+++ sql/fauna/fauna.sql
@@ -1,4 +1,4 @@
 select * from fauna where name LIKE '%fish%';
-select * from fauna where avg_age>=10000 AND avg_age<=20000;
+select * from fauna where avg_age>=10000 AND avg_age<=21000;
 select * from fauna where discovery_date IS NULL;
 select * from fauna where discovery_date < '1950-01-01';
