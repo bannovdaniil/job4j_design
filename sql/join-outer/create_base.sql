fbe2384292c3b6f129c878c6d5a9042b93d04f33
bannovdaniil <baddan@mail.ru>
2022-01-18 20:49:13 +0300

    0. Join [#6862]

---
+++ sql/join-outer/create_base.sql
@@ -0,0 +1,44 @@
+CREATE TABLE departments (
+  dep_id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE emploers (
+  emp_id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  dep_id INTEGER REFERENCES departments(dep_id)
+);
+
+INSERT INTO departments(name) VALUES('Финансовый'); -- 1
+INSERT INTO departments(name) VALUES('Информационный'); -- 2
+INSERT INTO departments(name) VALUES('Юридический'); -- 3
+INSERT INTO departments(name) VALUES('Маркетиг'); -- 4
+INSERT INTO departments(name) VALUES('Продажи'); -- 5
+INSERT INTO departments(name) VALUES('Транспортный'); -- 6
+INSERT INTO departments(name) VALUES('Технический'); -- 7
+
+INSERT INTO emploers(name, dep_id) VALUES('Марина', 1);
+INSERT INTO emploers(name, dep_id) VALUES('Наташа', 1);
+INSERT INTO emploers(name, dep_id) VALUES('Сережа', 2);
+INSERT INTO emploers(name, dep_id) VALUES('Никита', 2);
+INSERT INTO emploers(name, dep_id) VALUES('Коля', 4);
+INSERT INTO emploers(name, dep_id) VALUES('Света', 4);
+INSERT INTO emploers(name, dep_id) VALUES('Юля', 5);
+INSERT INTO emploers(name, dep_id) VALUES('Софья', 5);
+INSERT INTO emploers(name, dep_id) VALUES('Костя', 6);
+INSERT INTO emploers(name, dep_id) VALUES('Алексей', 6);
+INSERT INTO emploers(name, dep_id) VALUES('Гена', 6);
+INSERT INTO emploers(name, dep_id) VALUES('Андрей', 7);
+
+-- 5. Создать таблицу teens с атрибутами name, gender и заполнить ее. 
+-- Используя cross join составить все возможные разнополые пары
+CREATE TABLE teens (
+	id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+	gender VARCHAR(255)
+);                  
+
+INSERT INTO teens(name, gender) VALUES('Саша', 'мальчик');
+INSERT INTO teens(name, gender) VALUES('Женя', 'девочка');
+
+
---
+++ sql/join-outer/select.sql
@@ -0,0 +1,52 @@
+﻿-- 2. Выполнить запросы с left, rigth, full, cross соединениями
+SELECT *
+from departments AS dep
+LEFT OUTER JOIN emploers AS emp
+ON dep.dep_id = emp.dep_id
+ORDER BY dep.name;
+
+SELECT *
+from departments AS dep
+RIGHT OUTER JOIN emploers AS emp
+ON dep.dep_id = emp.dep_id
+ORDER BY dep.name;
+
+SELECT *
+from departments AS dep
+FULL OUTER JOIN emploers AS emp
+ON dep.dep_id = emp.dep_id
+ORDER BY dep.name;
+
+SELECT *
+from departments AS dep
+CROSS JOIN emploers AS emp
+ORDER BY dep.name;
+
+-- 3. Используя left join найти департаменты, у которых нет работников
+SELECT *
+from departments AS dep
+LEFT OUTER JOIN emploers AS emp
+ON dep.dep_id = emp.dep_id
+WHERE emp.dep_id is null
+ORDER BY dep.name;
+
+-- 4. Используя left и right join написать запросы, которые давали бы одинаковый результат 
+-- (порядок вывода колонок в эти запросах также должен быть идентичный). 
+SELECT *
+from departments AS dep
+LEFT OUTER JOIN emploers AS emp
+ON dep.dep_id = emp.dep_id
+ORDER BY dep.name;
+
+SELECT dep.dep_id, dep.name, emp.emp_id, emp.name, emp.dep_id
+from emploers AS emp
+RIGHT OUTER JOIN departments AS dep
+ON dep.dep_id = emp.dep_id
+ORDER BY dep.name;
+
+-- 5. Создать таблицу teens с атрибутами name, gender и заполнить ее. 
+-- Используя cross join составить все возможные разнополые пары
+SELECT t1.name, t2.gender
+FROM teens AS t1
+CROSS JOIN teens AS t2;
+
