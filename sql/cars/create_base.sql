b4089ae6111ce3d6a270343c1cbdb2ac19c4a914
bannovdaniil <baddan@mail.ru>
2022-01-18 22:00:36 +0300

    Хранилище машин [#1733]

---
+++ sql/cars/create_base.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE carbody(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255)
+);
+CREATE TABLE engine(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255)
+);
+CREATE TABLE gearbox(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255)
+);
+CREATE TABLE cars(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255),
+	carbody_id INTEGER REFERENCES carbody(id),
+	engine_id INTEGER REFERENCES engine(id),
+	gearbox_id INTEGER REFERENCES gearbox(id)
+);
+                              --    1          2             3         4            5              6
+INSERT INTO carbody(name) VALUES('седан'), ('пикап'), ('кабриолет'), ('купе'), ('универсал'), ('Хэтчбек');
+INSERT INTO engine(name) VALUES('бензиновый'), ('дизельный'), ('газовый'), ('электрический');
+INSERT INTO gearbox(name) VALUES('механическая'), ('автоматическая'), ('роботизированная'), ('вариативная');
+
+INSERT INTO 
+cars(name, carbody_id, engine_id, gearbox_id)
+VALUES
+('тойота',   1, 1, 1),
+('мерседес', 3, 1, 2),
+('бмв',      5, 2, 4),
+('тесла',    1, 4, 4),
+('ягуар',    1, 2, 4),
+('хонда',    2, 2, 4),
+('мустанг',  3, 2, 4),
+('киа',      1, 1, 1),
+('хюндай',   1, 2 ,4);
+
+
---
+++ sql/cars/select.sql
@@ -0,0 +1,30 @@
+﻿-- 1) Вывести список всех машин и все привязанные к ним детали.
+SELECT car.name, cb.name, eng.name, gb.name
+FROM cars AS car
+INNER JOIN carbody AS cb
+ON car.carbody_id = cb.id
+INNER JOIN engine AS eng
+ON car.engine_id = eng.id
+INNER JOIN gearbox AS gb
+ON car.gearbox_id = gb.id;
+
+-- 2) Вывести отдельно детали (1 деталь - 1 запрос), которые не используются НИ в одной машине, кузова, 
+-- двигатели, коробки передач.
+
+SELECT cb.id, cb.name
+FROM carbody AS cb
+LEFT OUTER JOIN cars AS car
+ON cb.id = car.carbody_id
+WHERE car.carbody_id IS NULL;
+
+SELECT eng.id, eng.name
+FROM engine AS eng
+LEFT OUTER JOIN cars AS car
+ON eng.id = car.carbody_id
+WHERE car.carbody_id IS NULL;
+
+SELECT gb.id, gb.name
+FROM gearbox AS gb
+LEFT OUTER JOIN cars AS car
+ON gb.id = car.carbody_id
+WHERE car.carbody_id IS NULL;
